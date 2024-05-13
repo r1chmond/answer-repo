ea5ecaac717cd20439752f2bef6e806608a9b6f4
richmond <richmond.bobie@outlook.com>
2024-05-13 12:50:07 +0100

    rename folder

--- answer-repo/wsgi.py
+++ answer-repo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for answer_repo project.
+WSGI config for answer-repo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'answer_repo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'answer-repo.settings')
 
 application = get_wsgi_application()
